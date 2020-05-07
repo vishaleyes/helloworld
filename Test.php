f117015c25cc2129184cb724c2e13ff62a625343
Vishal Panchal <vishal.panchal@bypt.in>
2020-05-07 12:53:51 +0530

    sdfasdf

--- Test.php
+++ Test.php
@@ -3,7 +3,5 @@ require_once 'vendor/autoload.php'; // Autoload files using Composer autoload
 use HelloWorld\SayHello;
 echo SayHello::world();
 
-echo "Automated Deploying";
-echo "Seecond STEP";
-echo "thiord STEP";
+
 echo "fourth STEP";
